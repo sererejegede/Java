af50649748830b15befd1cda59ebee78cfc9fc3e
Serere Jegede <sererejegede@gmail.com>
2017-11-15 14:30:50 +0100

    Added .jar
    The previously ingnored .jar is added

--- Sort/src/sort/Sort.java
+++ Sort/src/sort/Sort.java
@@ -4,20 +4,45 @@ package sort;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Sort {
-    int input;
+    String input;
+    String searchInput;
     
     public void inputAccept(){
         Scanner inputAcceptor = new Scanner(System.in);
-        System.out.println("Enter a list of numbers(integers), seperated by a space");
-        input = inputAcceptor.nextInt();
+        System.out.println("Type in anything");
+        input = inputAcceptor.nextLine();
+        System.out.println("Enter what to search for");
+        searchInput = inputAcceptor.nextLine();
+        
     }
+    public void search(String toSearch){
+        this.input = toSearch;
+        System.out.println(toSearch.indexOf(searchInput));
+        
+    }
+    public void inputToArray(){
+        char[] inputArray;
+        inputArray = input.toCharArray();
+        Arrays.sort(inputArray);
+        String sortedInput = Arrays.toString(inputArray);
+        System.out.println(sortedInput);
+        search(sortedInput);
+    }
+    
     
     public static void main(String[] args) {
-        int[] arr = {34, 22, 3, 45, 342, 213, 5};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        Sort newSort = new Sort();
+        newSort.inputAccept();
+        
+        newSort.inputToArray();
+        String inputo = newSort.input;
+        //newSort.search(inputo);
+//        if
     }
+
     
 }
-//  int[] arr = {34, 22, 3, 45, 342, 213, 5};
-//  Arrays.sort(arr);
+
+//        int[] arr = {34, 22, 3, 45, 342, 213, 5};
+//        Arrays.sort(arr);
+//        System.out.println(Arrays.toString(arr));
